c3feb2b5cba87657af179bfbfa35969e3d3da5bc
George Kaniouris <gkaniouris@gmail.com>
2018-02-10 18:45:36 +0000

    Added basic redis connection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
+const client = redis.createClient(process.env.REDIS_URL);
 
 app.get('/', (req, res) => {
   console.log('Received request');
-  res.send('Hello World!');
+  client.set('foo', 'bar');
+  client.get('foo', (err, data) => {
+    res.send(`Hello World! ${data}`);
+  });
 });
 
 app.listen(3000, () => {
