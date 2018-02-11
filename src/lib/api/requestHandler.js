db54e38fc6af6de7ed199581e97bd36fb45c4362
George Kaniouris <gkaniouris@gmail.com>
2018-02-11 18:31:28 +0000

    Removed console.log from requestHandler

--- src/lib/api/requestHandler.js
+++ src/lib/api/requestHandler.js
@@ -7,15 +7,11 @@ const requestHandler = {
   sendUnreliable: async (url, options, maxRetries = 5) => {
     let currentRetries = 0;
     while (currentRetries < maxRetries) {
-      console.log(`Request to ${url}. attempt: ${currentRetries}`);
       // eslint-disable-next-line no-await-in-loop
       const response = await httpClient(url, options);
       if (response.ok || currentRetries === maxRetries - 1) {
-        if (currentRetries !== maxRetries - 1) console.log('Response is ok');
-        else console.log('Response not ok but reached max retries');
         return response;
       }
-      console.log(`Response not ok: ${response.status} ${response.statusText}`);
       currentRetries += 1;
     }
     return null;
